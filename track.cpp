f8b06f59f4f6a31218a0223d9903db1120a7c2fe
Pavel Richtarik <palirichtarik@github.com>
2015-12-04 14:11:06 +0100

    ADD: track.h track.cpp

---
+++ track.cpp
@@ -0,0 +1,20 @@
+/*
+ * track.cpp
+ *
+ *  Created on: Dec 4, 2015
+ *      Author: pavel
+ */
+
+#include "track.h"
+
+CTrack::CTrack()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+CTrack::~CTrack()
+{
+    // TODO Auto-generated destructor stub
+}
+
---
+++ track.h
@@ -0,0 +1,20 @@
+/*
+ * track.h
+ *
+ *  Created on: Dec 4, 2015
+ *      Author: pavel
+ */
+
+#ifndef TRACK_H_
+#define TRACK_H_
+
+#include "simlib.h"
+
+class CTrack
+{
+public:
+    CTrack();
+    virtual ~CTrack();
+};
+
+#endif /* TRACK_H_ */
